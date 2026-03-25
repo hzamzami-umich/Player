b9c5ffa7b3fb3d4c2da9d7deae7a12b257d33680
Hebba <hzamzami@umich.edu>
2026-03-25 13:31:29 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Player
+{
+    partial class Player
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Player));
+            this.playerBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.playerBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playerBox
+            // 
+            this.playerBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.playerBox.BackColor = System.Drawing.Color.Transparent;
+            this.playerBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("playerBox.BackgroundImage")));
+            this.playerBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.playerBox.InitialImage = null;
+            this.playerBox.Location = new System.Drawing.Point(375, 375);
+            this.playerBox.Name = "playerBox";
+            this.playerBox.Size = new System.Drawing.Size(50, 50);
+            this.playerBox.TabIndex = 0;
+            this.playerBox.TabStop = false;
+            this.playerBox.LocationChanged += new System.EventHandler(this.playerBox_KeyDown);
+            // 
+            // Player
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 444);
+            this.Controls.Add(this.playerBox);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 500);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(800, 500);
+            this.Name = "Player";
+            this.Text = "Player";
+            ((System.ComponentModel.ISupportInitialize)(this.playerBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox playerBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Player
+{
+    public partial class Player : Form
+    {
+        public Player()
+        {
+            InitializeComponent();
+
+            playerBox = new PictureBox(); // Initialize the player box
+
+            playerBox.BackColor = Color.Red; // Set the color of the player box
+            playerBox.Size = new Size(50, 50); // Set the size of the player box
+            playerBox.Location = new Point(375, 375); // Set the initial position of the player box
+
+            Controls.Add(playerBox); // Add the player box to the form
+
+            KeyDown += playerBox_KeyDown; // Subscribe to the KeyDown event of the form
+        }
+
+        //private void Player_Load(object sender, EventArgs e)
+        //{
+        //    // Set the initial position of the player box
+        //    playerBox.Left = (this.ClientSize.Width - playerBox.Width) / 2;
+        //    playerBox.Top = (this.ClientSize.Height - playerBox.Height) / 2;
+        //}
+
+        //private void playerBox_KeyDown(object sender, KeyEventArgs e)
+        //{
+        //    playerBox.Focus(); // Ensure the player box has focus to receive key events
+        //    int moveAmount = 10; // Amount to move the player box
+
+        //    if (e.KeyCode == Keys.Left)
+        //    {
+        //        playerBox.Left -= moveAmount;
+        //    }
+
+        //    if (e.KeyCode == Keys.Right)
+        //    {
+        //        playerBox.Left += moveAmount;
+        //    }
+
+        //    //switch (e.KeyCode)
+        //    //{
+        //    //    case Keys.Left:
+        //    //        playerBox.Left -= moveAmount;
+        //    //        break;
+        //    //    case Keys.Right:
+        //    //        playerBox.Left += moveAmount;
+        //    //        break;
+        //    //}
+        //}
+
+        private void playerBox_KeyDown(object sender, EventArgs e)
+        {
+            playerBox.Refresh();
+
+            int moveAmount = 10;
+
+            if (playerBox.Location.X >= 1 && playerBox.Location.X <= 489)
+            {
+
+                if (e is KeyEventArgs keyEventArgs)
+                {
+                    if (keyEventArgs.KeyCode == Keys.Left)
+                    {
+                        playerBox.Left -= moveAmount;
+                        playerBox.Refresh(); // Refresh the player box to update its position
+                    }
+                    else if (keyEventArgs.KeyCode == Keys.Right)
+                    {
+                        playerBox.Left += moveAmount;
+                        playerBox.Refresh(); // Refresh the player box to update its position
+                    }
+
+                    playerBox.Refresh();
+
+                }
+            }
+
+            else
+            {
+                if (playerBox.Location.X < 1)
+                {
+                    playerBox.Left = 1; // Prevent moving left beyond the boundary
+                    playerBox.Refresh();
+                }
+                else if (playerBox.Location.X > 489)
+                {
+                    playerBox.Left = 489; // Prevent moving right beyond the boundary
+                    playerBox.Refresh();
+                }
+
+                playerBox.Refresh();
+
+            }
+        }
+
+        //public static void Main()
+        //{
+        //    Player player = new Player();
+
+        //    player.Show();
+        //    player.Width = 1000; // Set the width of the form
+        //    player.Height = 500; // Set the height of the form
+
+        //    Application.Run(new Player());
+        //}
+    }
+}
